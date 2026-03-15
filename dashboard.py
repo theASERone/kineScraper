413cb5d271e1c65ac5d9687e5108e63eb89069ea
theASERone <aitor1.atlet@gmail.com>
2026-03-15 15:26:26 +0100

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,31 +3,35 @@ import pandas as pd
 import json
 from streamlit_autorefresh import st_autorefresh
 
-# refrescar cada 10 segundos
+st.set_page_config(layout="wide")
+
 st_autorefresh(interval=10000, key="datarefresh")
 
 st.title("🎬 Ocupación Kinepolis")
 
 # ======================
-# METADATA
+# CARGAR DATOS
 # ======================
+
+df = pd.read_csv("ocupacion_kinepolis.csv")
 
 with open("metadata.json", "r", encoding="utf-8") as f:
     metadata = json.load(f)
 
-st.subheader("Último scrape")
+# ======================
+# MÉTRICAS
+# ======================
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 
 col1.metric("Inicio informe", metadata["inicio_informe"])
 col2.metric("Duración (s)", metadata["duracion_segundos"])
-col3.metric("Sesiones analizadas", metadata["sesiones_analizadas"])
+col3.metric("Sesiones", metadata["sesiones_analizadas"])
+col4.metric("Películas", df["pelicula"].nunique())
 
 # ======================
-# DATOS
+# TABLA
 # ======================
-
-df = pd.read_csv("ocupacion_kinepolis.csv")
 
 st.subheader("Datos por sesión")
 
@@ -37,6 +41,8 @@ st.dataframe(df)
 # GRÁFICO
 # ======================
 
-st.subheader("Ocupación por sesión")
+resumen = df.groupby("hora")[["ocupadas","total"]].sum().reset_index()
 
-st.bar_chart(df.set_index("hora")["ocupacion"])
+st.subheader("Butacas ocupadas por hora")
+
+st.bar_chart(resumen.set_index("hora")["ocupadas"])
